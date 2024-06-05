f615684eb2c65ea943123a1475eee1665ae12711
user <aqusay06@gmail.com>
2024-06-05 18:05:34 +0300

    edit data memory

--- PipelineProcessor/src/data_memory/DataMemory.v
+++ PipelineProcessor/src/data_memory/DataMemory.v
@@ -2,10 +2,10 @@ module DataMemory (
     input wire clk,
     input wire wrEnable,
     input wire rdEnable,
+	input wire [1:0]numberOfByte,
     input wire [15:0] address,
     input wire [15:0] in,
     output reg [15:0] out,
-    input wire numberOfByte
 );
 
     reg [7:0] memory [0:255]; 		// the size will be 2 ^ 16
@@ -26,12 +26,15 @@ module DataMemory (
 			
         end	else if (rdEnable) begin
 			
-            if (numberOfByte == 1) begin
+            if (numberOfByte == 2'b00) begin
                 // Reading 16-bit data from two consecutive memory locations
                 out = {memory[address + 1], memory[address]};
-            end else if (numberOfByte == 0) begin
+            end else if (numberOfByte == 2'b01) begin
                 // Reading 8-bit data from one memory location with MSB = 00000000
-                out = {8'b00000000, memory[address]};
+                out = {8'b00000000, memory[address]};	 
+			end else if (numberOfByte == 2'b01) begin
+                // Sign extend memory[address] and store to out
+            	out = {{8{memory[address][7]}}, memory[address]};
             end
 			
         end 
